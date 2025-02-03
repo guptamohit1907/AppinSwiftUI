bcbd87014db5cade7ce252f5f9ad03316fef79a0
Mohit Gupta <Mohit.Gupta14@ibm.com>
2025-02-04 00:03:51 +0530

    Initial Project Setup

---
+++ AppInSwiftUI/AppInSwiftUI/AppInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppInSwiftUIApp.swift
+//  AppInSwiftUI
+//
+//  Created by Mohit Gupta on 04/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AppInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
